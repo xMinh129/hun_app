ebb45f312b5dc88b05596b5d37bfd05ab5788505
Minh Vu <v.xuanminh@alibaba-inc.com>
2019-10-08 20:35:33 +0800

    fix image link

--- client/app/components/Home/Voting.js
+++ client/app/components/Home/Voting.js
@@ -21,7 +21,7 @@ class Voting extends Component {
         <div className="row">
           <div className="column-50-inline competition_1">
             <img
-              src={require('../../../public/assets/img/images/voting/hun_lin_info.JPG')}/>
+              src={require('../../../public/assets/img/images/voting/hun_lin_info.jpg')}/>
             <a href="http://vote.linnarrowthegap.org/" target="blank"><button className="btn btn-danger btn-lg rds btn_donate btn_join btn_voting_1" type="button" id="btn_join">Bình chọn cho Hear.Us.Now</button></a>
           </div>
           <div className="column-50-inline competition_2">
